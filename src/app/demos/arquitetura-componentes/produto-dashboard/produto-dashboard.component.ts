3f2ebe37961da5859d0dabe0bacde277b82778f4
Karen <karen_melo@live.com>
2023-12-01 18:53:51 -0300

    uso de @viewchild e @viewchildren

--- src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
+++ src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { Produto } from '../models/produto';
+import { Observable, fromEvent } from 'rxjs';
+import { ProdutoCountComponent } from '../componentes/produto-count.component';
+import { ProdutoDetalheComponent } from '../componentes/produto-card-detalhe.component';
 
 @Component({
   selector: 'app-produto-dashboard',
   templateUrl: './produto-dashboard.component.html',
   styleUrls: ['./produto-dashboard.component.css'],
 })
-export class ProdutoDashboardComponent implements OnInit {
+export class ProdutoDashboardComponent implements OnInit, AfterViewInit {
   produtos: Produto[];
-
+  @ViewChild(ProdutoCountComponent, { static: false })
+  contador: ProdutoCountComponent;
+  @ViewChild('teste', { static: false }) mensagemTela: ElementRef;
   constructor() {}
+  @ViewChildren(ProdutoDetalheComponent)
+  botoes: QueryList<ProdutoDetalheComponent>;
 
   ngOnInit(): void {
     this.produtos = [
@@ -56,6 +71,18 @@ export class ProdutoDashboardComponent implements OnInit {
         imagem: 'headset.jpg',
       },
     ];
+  }
+
+  ngAfterViewInit(): void {
+    console.log('Objeto do contator: ', this.contador.produtos);
+    let clickTexto: Observable<any> = fromEvent(
+      this.mensagemTela.nativeElement,
+      'click'
+    );
+    clickTexto.subscribe(() => {
+      alert('Clicou no texto!');
+      return;
+    });
   }
 
   mudarStatus(event: Produto) {
