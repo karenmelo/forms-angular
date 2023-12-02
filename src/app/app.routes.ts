fecb6aa25204a37fdd3e6cc323e6ff12c3198569
Karen <karen_melo@live.com>
2023-12-01 21:32:31 -0300

    Criacao de uma rota padrao do angular para routes desconhecidos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './navegacao/home/home.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
 import { NgModule } from '@angular/core';
+import { NotFoundComponent } from './navegacao/not-found/not-found.component';
 
 const rootRouterConfig: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -16,6 +17,7 @@ const rootRouterConfig: Routes = [
         (module) => module.ProdutoModule
       ),
   },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
--- src/app/navegacao/navegacao.module.ts
+++ src/app/navegacao/navegacao.module.ts
@@ -4,10 +4,16 @@ import { RouterModule } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [MenuComponent, HomeComponent, FooterComponent],
+  declarations: [
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    NotFoundComponent,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [MenuComponent, HomeComponent, FooterComponent],
+  exports: [MenuComponent, HomeComponent, FooterComponent, NotFoundComponent],
 })
 export class NavegacaoModule {}
